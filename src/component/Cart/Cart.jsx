eacb0d8054893a15dbf9de3c9d267dcd91b6c512
Abu-Sahad <abusahad603@gmail.com>
2023-05-03 12:55:06 +0600

    added toast

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -3,14 +3,19 @@ import './Cart.css';
 const Cart = ({ cartItems }) => {
     return (
         <div className='cart'>
+            <div className='spend-time'>
+                <h3>Spend Time On Read:</h3>
+            </div>
             <div>
                 <h2>Bookmarked Blogs: {cartItems.length}</h2>
             </div>
-            <div className='cart-details'>
+            <div className='cart-summary'>
                 {cartItems.map((product) => (
-                    <h3 key={product.id}>{product.discription}</h3>
+                    <h3 className='cart-details' key={product.id}>{product.discription}</h3>
                 ))}
             </div>
+
+
         </div>
     );
 };
