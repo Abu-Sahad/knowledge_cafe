3f041d7a96ad720b6ba04c54b9f53d67089da656
Abu-Sahad <abusahad603@gmail.com>
2023-05-03 13:11:04 +0600

    spend of time added

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import './Cart.css';
 
 const Cart = ({ cartItems }) => {
+    const totalReadTime = cartItems.reduce((acc, curr) => acc + parseInt(curr.read_time), 0);
+
     return (
         <div className='cart'>
             <div className='spend-time'>
-                <h3>Spend Time On Read:</h3>
+                <h3>Spend Time On Read: {totalReadTime} min</h3>
             </div>
             <div>
                 <h2>Bookmarked Blogs: {cartItems.length}</h2>
