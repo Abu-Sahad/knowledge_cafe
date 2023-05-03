561d889672ed4befe95d4bbd796e284c346ed21f
Abu-Sahad <abusahad603@gmail.com>
2023-05-03 22:01:19 +0600

    added complete all part

--- src/component/Blogs/Blogs.jsx
+++ src/component/Blogs/Blogs.jsx
@@ -1,65 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import Blog from '../Blog/Blog';
-// import './Blogs.css'
-// import Cart from '../Cart/Cart';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
-// const Blogs = () => {
-//   const [blogs, setBlogs] = useState([]);
-//   const [cart, setCart] = useState([]);
-//   const [addedProductIds, setAddedProductIds] = useState([]);
-//   const [totalReadTime, setTotalReadTime] = useState([]);
-
-//   useEffect(() => {
-//     fetch('knowledge.json')
-//       .then(res => res.json())
-//       .then(data => setBlogs(data))
-//   }, []);
-
-//   const handleClick = (product) => {
-//     if (addedProductIds.includes(product.id)) {
-//       toast.warning(`"${product.discription}" is already in the cart.`, {
-//         position: toast.POSITION.TOP_CENTER,
-//         autoClose: 2000,
-//       });
-//       return;
-//     }
-//     setAddedProductIds([...addedProductIds, product.id]);
-//     setCart([...cart, product]);
-//   }
-//   const cartItems = cart.filter(item => addedProductIds.includes(item.id));
-
-//   const handleTimeToClick = (product) => {
-//     const cartItems = cart.filter(item => addedProductIds.includes(item.id));
-//     const newTotalReadTime = cartItems.reduce((acc, curr) => acc + parseInt(curr.read_time), 0);
-//     setTotalReadTime(newTotalReadTime);
-//   }
-
-//   return (
-//     <div className='all-blog'>
-//       <div className='blog-part'>
-//         {
-//           blogs.map(product => (
-//             <Blog
-//               product={product}
-//               key={product.id}
-//               handleClick={handleClick}
-//               handleTimeToClick={handleTimeToClick}
-//             />
-//           ))
-//         }
-//       </div>
-//       <div className='blog-part-second'>
-//         <Cart cartItems={cartItems} totalReadTime={totalReadTime} />
-//       </div>
-//       <ToastContainer />
-//     </div>
-//   );
-// };
-
-// export default Blogs;
-
 import React, { useEffect, useState } from 'react';
 import Blog from '../Blog/Blog';
 import './Blogs.css'
@@ -70,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
   const [cart, setCart] = useState([]);
-  const [cartTwo,setCartTwo]=useState([])
+  const [cartTwo, setCartTwo] = useState([])
   const [addedProductIds, setAddedProductIds] = useState([]);
   const [totalReadTime, setTotalReadTime] = useState(0);
 
