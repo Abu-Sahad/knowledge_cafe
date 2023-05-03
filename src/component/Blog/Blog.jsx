60235909e02d2b61c3ec0105ee0f43c9968343ca
Abu-Sahad <abusahad603@gmail.com>
2023-05-03 10:12:11 +0600

    banner-area left side done

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Blog.css'
 const Blog = (props) => {
-    const { name, img, date, discription, person_img } = props.product
+    const { name, img, date, discription, person_img, read_time } = props.product
     return (
         <div className='blog-details'>
             <img src={img} alt="" />
             <div className='person-details'>
                 <div className='single-person'>
-                     <div>
+                    <div>
                         <img src={person_img} alt="" />
-                     </div>
-                     <div className='single-person-details'>
-                          <h5>{name}</h5>
-                          <p>{date}</p>
-                     </div>
+                    </div>
+                    <div className='single-person-details'>
+                        <h5>{name}</h5>
+                        <p>{date}</p>
+                    </div>
                 </div>
                 <div className='read-time-area'>
-                    <p>05 min Read</p>
+                    <p>{read_time}</p>
                 </div>
             </div>
+            <h1>{discription}</h1>
+            <a href="">Mark As Read</a>
         </div>
     );
 };
